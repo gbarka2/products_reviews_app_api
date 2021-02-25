279af93a4c4a82fa36365a8cd753edb3bde91af4
Gary Barkauski <gbarka2@gmail.com>
2021-02-24 19:44:38 -0700

    Products controller fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,16 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
 
   # GET /products
   def index
     @products = Product.all
 
-    render json: @products
+    render json: @products, status: 200
   end
 
   # GET /products/1
   def show
-    render json: @product
+
+    render json: @product, status: 200
   end
 
   private
@@ -18,5 +18,5 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
-    
+
 end
