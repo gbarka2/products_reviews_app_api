58815bb320da707e626ee7724af079bc1b430629
Gary Barkauski <gbarka2@gmail.com>
2021-02-24 19:49:31 -0700

    Commit 7: Reviews index method fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :update, :destroy]
+  before_action :set_review, only: [:update, :destroy]
 
   # GET /reviews
   def index
-    @reviews = Review.all
+    reviews = Review.where(product_id: params[:product_id])
 
-    render json: @reviews
+    render json: reviews
   end
 
   # POST /reviews
